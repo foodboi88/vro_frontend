7b2041553d3306eccea910baefc065fd85f809d7
DESKTOP-2SKUUVU\admin <khacvole@gmail.com>
2023-06-11 21:37:12 +0700

    update

--- src/enum/api.enum.ts
+++ src/enum/api.enum.ts
@@ -1,5 +1,5 @@
 export enum API_URL {
-    HOST = "http://14.248.19.122:6068",
+    HOST = "http://vroteam.online:6068",
     GET_LATEST_SKETCH = "products/home/latest",
     GET_MOST_VIEWS_SKETCH = "products/home/most-views",
     GET_ALL_TOOLS = "design-tools",
